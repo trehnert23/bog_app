bc23b45f31640f336399292d2ed5289f89959c41
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-20 22:11:20 -0700

    section 19

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root to: 'creatures#index'
 
 
-  get '/creatures', to: 'creatures#index'
+  get '/creatures', to: 'creatures#index', as: 'creatures'
 
-  get '/creatures/new', to: 'creatures#new'
+  get '/creatures/new', to: 'creatures#new', as: 'new_creature'
 
-  get '/creatures/:id', to: 'creatures#show'
+  get '/creatures/:id', to: 'creatures#show', as: 'creature'
 
-  get '/creatures/:id/edit', to: 'creatures#show'
+  get '/creatures/:id/edit', to: 'creatures#show', as: 'edit_creature'
 
   post '/creatures', to: 'creatures#create'
 
