031fd2a5ce320e0f62b4ed28b8303e3256a7eae2
Thomas Rehnert <tomrehnert2011@gmail.com>
2015-09-20 22:03:02 -0700

    section 15

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get '/creatures', to: 'creatures#index'
   get '/creatures/new', to: 'creatures#new'
+
+  get '/creatures/:id', to: 'creatures#show'
   post '/creatures', to: 'creatures#create'
+
 
 end
